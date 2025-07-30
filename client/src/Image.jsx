451a3f4b32201ed69a2b8e59a6d13c4cb2e86902
mounikalingala <mounikalingala@gmail.com>
2025-07-30 21:31:19 +0530

    updating upload images

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,7 +1,7 @@
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
 export default function Image({ src, ...rest }) {
   src = src && src.includes('https://')
-    ? src : `${baseUrl}/uploads/ + ${src}`;
+    ? src : `${baseUrl}/uploads/${src}`;
   return (
     <img {...rest} src={src} alt={''} />
   );
